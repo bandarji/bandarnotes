0b115204701dfb52c80b7c4580efefe35d7bcba3
Sean Ellis <sellis@twitter.com>
2018-12-04 10:59:40 -0800

    primes

--- python/reddit/primes_to_100.py
+++ python/reddit/primes_to_100.py
@@ -1,24 +1,14 @@
-# Primes To 100
-
-```python
-import math
-
-def isprime(number: int) -> bool:
-    prime = True # assume it's a prime
-    if not isinstance(number, int):
-        prime = False # we only want to test integers
+def isprime(number):
+    if number < 2:
+        prime = False
     else:
-        if number < 2:
-            prime = False
-        else:
-            # The square root of the number, rounded up, is a valid final
-            # check. The all() will return True if all division tests return
-            # a remainder.
-            highest_check = int(math.sqrt(number) + 1)
-            prime = all(number % i for i in range(2, highest_check))
+        # The square root of the number, rounded up, is a valid final
+        # check. The all() will return True if all division tests return
+        # a remainder.
+        highest_check = int(number ** 0.5 + 1)
+        prime = all(number % i for i in range(2, highest_check))
     return prime
 
 # print all primes between 0 and 100
 for i in [x for x in range(101) if isprime(x)]:
     print(i)
-```
